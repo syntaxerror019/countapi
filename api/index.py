f0ef680638ea9e88176f919f7bb724dc41a9ec36
Miles Hilliard <skywiredvt@gmail.com>
2025-08-22 12:19:00 -0400

    update html

--- api/index.py
+++ api/index.py
@@ -65,7 +65,7 @@ def get_handler(key):
     value = r.get(key)
     if value is None:
         return jsonify({"error": "Key not found"}), 404
-    return jsonify({"message": "Key requested successfully", "key": key, "value": value}), 200
+    return jsonify({"message": "Key requested successfully", "key": key, "value": int(value)}), 200
 
 @app.route('/api/v1/set/<key>', methods=['GET'])
 def set_handler(key):
@@ -77,17 +77,17 @@ def set_handler(key):
 
     old_value = r.get(key)
     r.set(key, value)
-    return jsonify({"message": "Key set successfully", "key": key, "value": value, "old_value": old_value}), 200
+    return jsonify({"message": "Key set successfully", "key": key, "value": int(value), "old_value": int(old_value)}), 200
 
 @app.route('/api/v1/hit/<key>', methods=['GET'])
 def hit_handler(key):
     new_value = r.increase(key)
-    return jsonify({"message": "Key updated successfully", "key": key, "value": new_value}), 200
+    return jsonify({"message": "Key updated successfully", "key": key, "value": int(new_value)}), 200
 
-@app.route('/api/v1/status/memory', methods=['GET'])
+@app.route('/api/v1/status', methods=['GET'])
 def status_memory_handler():
-    mem_info = r.client.info('memory')
-    return jsonify({"message": "Memory status", "used_memory": mem_info}), 200
+    info = r.client.info()
+    return jsonify(info), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
